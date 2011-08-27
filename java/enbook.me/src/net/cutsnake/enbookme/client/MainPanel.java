e7117874122094b0105411cfe8d32b3216a54394
Jamie Burrell <lizard@cutsnake.net>
2011-08-27 16:07:24 +0100

    Added basic input validation

--- java/enbook.me/src/net/cutsnake/enbookme/client/MainPanel.java
+++ java/enbook.me/src/net/cutsnake/enbookme/client/MainPanel.java
@@ -9,6 +9,7 @@ import net.cutsnake.enbookme.shared.Book;
 
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.regexp.shared.RegExp;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
 import com.google.gwt.uibinder.client.UiHandler;
@@ -29,14 +30,16 @@ public class MainPanel extends Composite implements UserLoggedInEvent.Handler {
 
   private static MainPanelUiBinder uiBinder = GWT.create(MainPanelUiBinder.class);
   private final BookServiceAsync bookService;
-
+  private static final String URL_REGEX = "^(https?|ftp|file)://[-a-zA-Z0-9+&@#/%?=~_|!:,.;]*[-a-zA-Z0-9+&@#/%=~_|]";
   interface MainPanelUiBinder extends UiBinder<Widget, MainPanel> {}
-  
+
   @UiField(provided = true) CellTable<Book> bookList;
   @UiField TextBox urlBox;
   @UiField TextBox nameBox;
   @UiField TextBox emailBox;
-  
+  private String userEmail;
+  private RegExp urlRegex = RegExp.compile(URL_REGEX);
+
   public MainPanel(BookServiceAsync bookService) {
     this.bookService = bookService;
     this.bookList = new CellTable<Book>(10, Resources.INSTANCE);
@@ -79,7 +82,7 @@ public class MainPanel extends Composite implements UserLoggedInEvent.Handler {
       }
     }, "Last Updated");
     initWidget(uiBinder.createAndBindUi(this));
-    
+
     nameBox.getElement().setAttribute("placeholder", "the name the file should be sent as");
     urlBox.getElement().setAttribute("placeholder", "the URL of a document you'd like to read");
     emailBox.getElement().setAttribute("placeholder", "the email of your Kindle device");
@@ -87,8 +90,9 @@ public class MainPanel extends Composite implements UserLoggedInEvent.Handler {
 
   @Override
   public void handle(UserLoggedInEvent e) {
+    userEmail = e.getLoginInfo().getEmail();
     bookService.list(new AsyncCallback<List<Book>>() {
-      
+
       @Override
       public void onSuccess(List<Book> result) {
         setBooks(result);
@@ -101,27 +105,37 @@ public class MainPanel extends Composite implements UserLoggedInEvent.Handler {
       }
     });
   }
-  
+
   private void setBooks(List<Book> result) {
     bookList.setVisible(true);
     bookList.setRowData(result);
   }
-  
+
   @UiHandler("create")
   public void onCreate(ClickEvent e) {
-    Book book = new Book();
-    book.setName(nameBox.getText());
-    book.setUrl(urlBox.getText());
-    book.setEmail(emailBox.getText());
-    bookService.add(book, new AsyncCallback<List<Book>>() {
-      @Override
-      public void onFailure(Throwable caught) {
-      }
+    String emailAddr = emailBox.getText();
+    if (!emailAddr.endsWith("@kindle.com") && !emailAddr.endsWith("@free.kindle.com")
+            && !emailAddr.equalsIgnoreCase(userEmail)) {
+      Window.alert("Please only use Kindle device emails or the email registered to this account.");
+    } else if (!urlRegex.test(urlBox.getText())) {
+      Window.alert("Please use a valid URL");
+    } else {
+      Book book = new Book();
+      book.setName(nameBox.getText());
+      book.setUrl(urlBox.getText());
+      book.setEmail(emailAddr);
+      bookService.add(book, new AsyncCallback<List<Book>>() {
+        @Override
+        public void onFailure(Throwable caught) {}
 
-      @Override
-      public void onSuccess(List<Book> result) {
-        setBooks(result);
-      }
-    });
+        @Override
+        public void onSuccess(List<Book> result) {
+          setBooks(result);
+        }
+      });
+      nameBox.setText(null);
+      urlBox.setText(null);
+      emailBox.setText(null);
+    }
   }
 }
